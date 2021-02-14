4036eaf8b3e84da4485b4e8a0d314af0be9485c7
Zeenat Ali <zeenatali5710@gmail.com>
2021-02-14 15:35:30 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import React, { Component } from 'react';
+import Movies from "./Components/movies";
+
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
